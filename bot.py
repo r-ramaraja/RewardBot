0720facb252b22cf7274af7dae96cf475ba38611
Ramnath Raghu <ramnathraghu@Ramnaths-MacBook-Air.local>
2023-04-06 12:50:28 -0400

    Modified the code to support slack bolt

--- bot.py
+++ bot.py
@@ -1,86 +1,53 @@
-import slack
+from slack_bolt import App,request
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask, request, Response
-from slackeventsapi import SlackEventAdapter
-import mysql.connector
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.errors import SlackApiError
+import pymysql
+import threading
 
+reward_counts = {}
+lock = threading.Lock()
 
 env_path = Path('.') / '.env'
-
 load_dotenv(dotenv_path=env_path)
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-BOT_ID = client.api_call("auth.test")['user_id']
+app = App(token=os.environ['SLACK_TOKEN'], signing_secret=os.environ['SIGNING_SECRET'])
 
+BOT_ID = app.client.users_info(user=app.client.auth_test()['user_id'])['user']['id']
 
-# message_counts = {}
-reward_counts = {}
-
-mydb = mysql.connector.connect(
+mydb = pymysql.connect(
   host="localhost",
   user="root",
   password=os.environ['DATABASE_PWD'],
-  database="employee_database"
-)
+  database="employee_database")
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("SELECT * FROM EMPLOYEE_DETAILS")
-
-# myresult = mycursor.fetchall()
-
-# for x in myresult:
-#   print(x)
 
 
-# @slack_event_adapter.on('message')
-# def message(payload):
-#     event = payload.get('event',{})
-#     channel_id = event.get('channel')
-#     user_id = event.get('user')
-#     text = event.get('text')
-#     if BOT_ID != user_id:
-#         if user_id in message_counts:
-#             message_counts[user_id]+=1
-#         else:
-#             message_counts[user_id] = 1
-#         client.chat_postMessage(channel='#test', text=text)
-    
-# @app.route('/message-count', methods=['POST'])
-# def message_count():
-#     data = request.form
-#     user_id = data.get('user_id')
-#     channel_id = data.get('channel_id')
-#     message_count = message_counts.get(user_id,0)
-#     client.chat_postMessage(channel=channel_id, text=f"The message count of the user is: {message_count}")
-#     return Response(), 200
-
-@app.route('/start', methods=['POST'])
-def bot_start():
-    data = request.form
-    user_name= data.get('user_name')
-    channel_id = data.get('channel_id')
-    channel_name = data.get('channel_name')
-    print(data)
+@app.command("/start")
+def bot_start(ack, body, logger):
+    user_name = body['user_name']
+    channel_id = body['channel_id']
+    channel_name = body['channel_name']
     if channel_name == "reward":
-        client.chat_postMessage(channel=channel_id, text=f"Welcome to this channel {user_name}. You can provide your points to other employees by using the slash command /reward followed by the user_name and the reason you are awarding the points.\nExample) /reward <user_name> Helped me in solving a bug")
+        app.client.chat_postMessage(channel=channel_id, text=f"Welcome to this channel {user_name}. You can provide your points to other employees by using the slash command /reward followed by the user_name and the reason you are awarding the points.\nExample) /reward <user_name> Helped me in solving a bug")
     else:
-        client.chat_postMessage(channel=channel_id, text=f"Welcome to this channel {user_name}")
-    return Response(), 200
+        app.client.chat_postMessage(channel=channel_id, text=f"Welcome to this channel {user_name}.")
+    # Send an acknowledgement response to the Slack API
+    ack()
 
-@app.route("/reward", methods=["POST"])
-def handle_command():
+@ app.command("/reward")
+def handle_command(ack, respond, command):
     # Get the text following the slash command
-    data = request.form
-    command_text = request.form["text"]
-    user_name= data.get('user_name')
-    channel_id = data.get('channel_id')
-    channel_name = data.get('channel_name')
+    user_name = command['user_name']
+    channel_id = command['channel_id']
+    channel_name = command['channel_name']
+    command_text = command['text']
     employee_rewarded = command_text.split(" ", 1)[0]
+
     if len(command_text.split(" ", 1)) > 1:
         reward_reason = command_text.split(" ", 1)[1]
     else:
@@ -90,40 +57,59 @@ def handle_command():
     # ...
     # Respond to the user
     if channel_name == "reward":
-        mycursor.execute("SELECT points FROM EMPLOYEE_DETAILS WHERE user_name = %s", (employee_rewarded,))
+        mycursor.execute(
+            "SELECT points FROM EMPLOYEE_DETAILS WHERE user_name = %s", (employee_rewarded,))
         user = mycursor.fetchone()
         if not employee_rewarded:
-            client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\nEmployee Name should not be empty! Please Enter a name. For more details, give the slash command /start")
+            respond(
+                f"Input Text received: {command_text}.\nEmployee Name should not be empty! Please Enter a name. For more details, give the slash command /start")
         elif not user:
-            client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\nNo Such employee present. Try again")
+            respond(
+                f"Input Text received: {command_text}.\nI could not find '{employee_rewarded}' in our Employee Database. Try again")
         else:
             if user_name.lower() == employee_rewarded.lower():
-                client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\nYou should not reward points to yourself")
-            elif reward_reason=="":
-                client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\nPlease Justify why you are giving the point")
+                respond(
+                    f"Input Text received: {command_text}.\nYou should not reward points to yourself")
+            elif reward_reason == "":
+                respond(
+                    f"Input Text received: {command_text}.\nPlease Justify why you are giving the point")
             elif len(reward_reason.split(" ")) == 1:
-                client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\nOne word Justification is not sufficient. Please Elaborate the contribution")
+                respond(
+                    f"Input Text received: {command_text}.\nOne word Justification is not sufficient. Please Elaborate the contribution")
             else:
-                if user_name in reward_counts:
-                    reward_counts[user_name]+=1
+                mycursor.execute(
+                    "SELECT attempts FROM audit WHERE user_name = %s", (user_name,))
+                reward_count = mycursor.fetchone()[0]
+                if(reward_count >= 3):
+                    respond(
+                        f"Input Text received: {command_text}.\nYou can give points only for a maximum number of 3 times.")
                 else:
-                    reward_counts[user_name] = 1
-                reward_count = reward_counts.get(user_name,0)
-                if(reward_count>3):
-                    client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\nYou can give points only for a maximum number of 3 times.")
-                else:
-                    sql = "UPDATE EMPLOYEE_DETAILS SET points = %s WHERE user_name = %s"
-                    val = (user[0]+20, employee_rewarded)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                    client.chat_postMessage(channel=channel_id,text=f"Input Text received: {command_text}.\n20 points is rewarded to the employee '{employee_rewarded}'. The comment you gave is '{reward_reason}'")
+                    lock.acquire()
+                    try:
+                        sql = "UPDATE EMPLOYEE_DETAILS SET points = %s WHERE user_name = %s"
+                        val = (user[0]+20, employee_rewarded)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+                        sql2 = "UPDATE audit SET attempts = %s WHERE user_name = %s"
+                        val2 = (reward_count+1, user_name)
+                        mycursor.execute(sql2, val2)
+                        mydb.commit()
+                        respond(
+                            f"Input Text received: {command_text}.\n20 points is rewarded to the employee '{employee_rewarded}'. The comment you gave is '{reward_reason}'")
+                    except SlackApiError as e:
+                        print("Error: {}".format(e))
+                    except pymysql.Error as e:
+                        print("Error: {}".format(e))
+                        mydb.rollback()
+                    finally:
+                        # Release lock when finished accessing database
+                        lock.release()
     else:
-        client.chat_postMessage(channel=channel_id,text="Wrong channel.If you are willing to reward another employee, use the reward channel")
-    return Response(), 200
+        respond("Wrong channel.If you are willing to reward another employee, use the reward channel")
+
+    ack()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
-
-
-    #
+    handler = SocketModeHandler(app = app, app_token=os.environ['SLACK_APP_TOKEN'])
+    handler.start()
