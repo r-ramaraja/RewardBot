4c0cff62744d4890811be626a5c1787f853c3fac
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-04-21 14:27:28 -0400

    Made the changes according to the peer review

--- github_automation.py
+++ github_automation.py
@@ -6,66 +6,55 @@ from flask import Flask
 import os
 from pathlib import Path
 import pymysql
+from dotenv import load_dotenv
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
-#slack hook
-web_hook = 'https://hooks.slack.com/services/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'
+web_hook = os.getenv('web_hook')
 
 conn= pymysql.connect(
-    host= 'localhost',
-    user= 'root',
-    password= 'apple mango',
-    db= 'mydb',
+    host= os.getenv('host'),
+    user= os.getenv('user'),
+    password= os.getenv('password'),
+    db= os.getenv('db'),
     charset= 'utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
-    #unix_socket= "/tmp/mysql.sock"
-    #port= 5400
 )
 mycursor=conn.cursor()
-
-@app.route('/')
-def api_root():
-    return 'Hello' 
-
 
 
 @app.route('/github', methods=['POST'])
 def github():
     if request.headers['Content-Type']=='application/json':
             
-        
-        resp= request.json
+        resp = request.json
         
         action = resp['action']
 
-        if (action == 'closed'):
-            pr = resp['pull_request']   # next  5lines -> path to login i.e Github username
+        if (action == 'closed' and resp['pull_request']['merged']):
+            #extracting the username from the pull request so that points can be awarded to that user
+            pr = resp['pull_request']   
             base = pr['base']
             repo = base['repo']
             owner = repo['owner']
             login = owner['login']
             print('login:' ,login, '\n \n \n')
             
-            
+            #updating the database
             with conn.cursor() as cursor:
                 sql = "UPDATE EMP SET points = points + 20 WHERE ID = %s"
                 val = login
                 cursor.execute(sql, val)
                 conn.commit()
-                mycursor.execute("SELECT * FROM EMP WHERE ID =  %s", val)
-                myresult = mycursor.fetchall()
-                print(myresult)
                 
-                #using the webooks print
+                #using the slack webhook to display the messages on the Slack Channel
                 slack_msg = {'text': '20 points are rewarded to the employee', 'id':login}
                 requests.post(web_hook, data=json.dumps(slack_msg))
                 id_msg = {'text': login}
                 requests.post(web_hook, data=json.dumps(id_msg))
-        
-        return 'login'
-       
-        
-           # return 'ret'
+    return 'login'    
 if __name__=='__main__':
     app.run()
