1a7c29c3d3f8358ccf7b47911aa894b6ef9356b4
Ramaraja <r.ramaraja@yahoo.com>
2023-04-21 01:35:14 -0400

    Improve leaderboard feture

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import load_dotenv
 from slack.award_points_events import construct_award_points_events
+from slack.leaderboard_events import construct_leaderboard_events
 from settings import mysql_settings
 
 mysql_settings.init()
@@ -19,7 +20,7 @@ app = App(
 )
 
 app = construct_award_points_events(app, mysql_connection)
-
+app = construct_leaderboard_events(app, mysql_connection)
 
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
