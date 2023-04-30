b79b2129ee6e7e6cefa3de4261d82bfef7683955
Ramaraja <r.ramaraja@yahoo.com>
2023-04-30 02:34:55 -0400

    Enable MySQL autocommit

--- settings/mysql_settings.py
+++ settings/mysql_settings.py
@@ -14,7 +14,8 @@ def init():
         database=os.getenv("MYSQL_DATABASE"),
         user=os.getenv("MYSQL_USERNAME"),
         password=os.getenv("MYSQL_PASSWORD"),
-        port=os.getenv("MYSQL_PORT")
+        port=os.getenv("MYSQL_PORT"),
+        autocommit=True
     )
 
     try:
