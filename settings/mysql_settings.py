945ee117a64a6471de32372f1d035d1a0a88552a
Ramaraja <r.ramaraja@yahoo.com>
2023-04-30 01:45:35 -0400

    Add MYSQL_PORT env var

--- settings/mysql_settings.py
+++ settings/mysql_settings.py
@@ -15,6 +15,7 @@ def init():
             database=os.getenv("MYSQL_DATABASE"),
             user=os.getenv("MYSQL_USERNAME"),
             password=os.getenv("MYSQL_PASSWORD"),
+            port=os.getenv("MYSQL_PORT")
         )
 
     try:
