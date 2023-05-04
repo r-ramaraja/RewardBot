32bafeffb27cccb52e33540158e016be093447dd
Shaunak Juvekar <shaunakjuvekar@Shaunaks-MacBook-Air.local>
2023-05-04 15:05:13 -0400

    added unit tests for leaderboard

--- services/test_leaderboard_service.py
+++ services/test_leaderboard_service.py
@@ -1,16 +1,6 @@
-#def test_foo():
-   #pass
-
-
-''' 
-in the function ->  display_leaderboard()
-1. check validity of user
-2. 
-
-To write unit tests using Pytest, we will need to create mock objects for the function arguments ack, body, 
-client, and mysql_connection. We can use the pytest-mock library to easily create these mocks.
-
-Here's an example of how we can write unit tests for the display_leaderboard function:
+'''
+To write unit tests using Pytest, we are creating mock objects for the function arguments ack, body, 
+client, and mysql_connection. 
 
 '''
 import pytest
@@ -40,10 +30,8 @@ def test_display_leaderboard(mock_ack, mock_body, mock_client, mock_mysql_connec
     mock_cursor = mock_mysql_connection.cursor.return_value
     mock_cursor.fetchall.return_value = [("Martha", 100), ("Keith", 50), ("Brad", 25)]
 
-    # Function call
     leaderboard_service.display_leaderboard(mock_ack, mock_body, mock_client, mock_mysql_connection)
 
-    # Assert
     mock_ack.assert_called_once()
     mock_mysql_connection.cursor.assert_called_once()
     mock_cursor.execute.assert_called_once_with("SELECT full_name, points FROM employee ORDER BY points DESC LIMIT %s", (10,))
@@ -99,16 +87,3 @@ def test_display_leaderboard(mock_ack, mock_body, mock_client, mock_mysql_connec
     )
 
 
-'''
-
-In this test case, we use the pytest.fixture decorator to create mock objects for the function arguments ack, body, 
-client, and mysql_connection. We also use the mocker fixture provided by Pytest to patch 
-the mysql_connection.cursor method and set up the return value for the fetchall method.
-
-We then call the display_leaderboard function with the mock arguments, and assert that the ack method 
-is called, the MySQL query is executed correctly, and the chat_postMessage method is called with the 
-expected parameters.
-
-Note that we don't need to test the logic of the display_leaderboard function itself, since that logic 
-is relatively straightforward and can be verified by visual inspection. 
-'''
